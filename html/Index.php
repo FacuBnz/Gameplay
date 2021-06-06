5428b4a119791919b2d6b7b027e7e864dd893036
FacuBnz <facu.j.benitez@gmail.com>
2021-06-06 16:31:09 -0300

    Se creo la maqueta index para la vista

---
+++ html/Index.php
@@ -0,0 +1,28 @@
+<?php
+include_once(__DIR__."/layout/header.php");
+include_once(__DIR__."/layout/sidebar.php");
+?>
+    <div id="principal">
+        <h1>Ultimas publicaciones</h1>
+
+        <?php foreach ($this->publicaciones as $p): ?>
+            <article>
+                <a href="#">
+                    <h2><?=$p['titulo']?></h2>
+                    <span class="fecha"><?=$p['fecha']?> | <?=$p['nombre']?></span>
+                    <p><?=$p['descripcion']?></p>
+                </a>
+            </article>
+        <?php endforeach;?>
+
+
+
+        <div id="ver-todas">
+            <a href="#">Ver todas</a>
+        </div>
+    </div>
+</main>
+
+<?php
+include_once(__DIR__."/layout/footer.php");
+
