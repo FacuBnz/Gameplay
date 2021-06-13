12bbed4528e1daa56b3f44766d8bb8d145175f9e
FacuBnz <facu.j.benitez@gmail.com>
2021-06-13 19:21:30 -0300

    Se agregó el metodo GetNameCategory

--- models/Categorias.php
+++ models/Categorias.php
@@ -6,4 +6,20 @@ class Categorias extends Model{
         $this->db->query("SELECT * FROM categorias");
         return $this->db->fetchAll();
     }
+
+    public function getNameCategory($id){
+
+        if(ctype_digit($id)){
+            $sql = "SELECT id, nombre FROM categorias WHERE id=$id";
+
+            $this->db->query($sql);
+            $rs = $this->db->fetch();
+            if($rs) return $rs;
+            header('Location: index.php');
+            
+        }
+        header('Location: index.php');
+    }
+
+    
 }
