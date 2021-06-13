50e8cce0b0ec2e8ae545ef459a40fbfc4fdc55e4
FacuBnz <facu.j.benitez@gmail.com>
2021-06-13 20:55:14 -0300

    Se creo el controlador de una publicacion

---
+++ controllers/publicacion.php
@@ -0,0 +1,19 @@
+<?php
+
+require '../fw/fw.php';
+require '../models/Categorias.php';
+require '../models/Publicaciones.php';
+require '../views/Publicacion.php';
+
+$cate = new Categorias();
+$cateTodos = $cate->getTodos();
+
+$publi = new Publicaciones();
+$publi = $publi->getPublicacion($_GET['titulo']);
+
+//var_dump($publi);
+
+$v = new Publicacion();
+$v->categorias = $cateTodos;
+$v->publicacion = $publi;
+$v->render();
