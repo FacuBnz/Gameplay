290e2d15abd2c58cb0b92fa5e326971511c13980
FacuBnz <facu.j.benitez@gmail.com>
2021-06-13 20:59:28 -0300

    Se agrego un metodo que consigue una publicacion

--- models/Publicaciones.php
+++ models/Publicaciones.php
@@ -15,4 +15,23 @@ class Publicaciones extends Model{
         $this->db->query($sql);
         return $this->db->fetchAll();
     }
+
+    public function getPublicacion($tittle){
+
+        if(empty($tittle)) header('Location: index.php');
+
+        $tittle = $this->db->escape($tittle);
+        $tittle = $this->db->escapeWildcards($tittle);
+
+        $sql = "SELECT p.*, c.nombre 
+                FROM publicaciones p 
+                INNER JOIN categorias c 
+                ON p.categoria_id = c.id
+                WHERE p.titulo = \"$tittle\" 
+                ORDER BY p.id DESC";
+
+        $this->db->query($sql);
+        return $this->db->fetch();
+
+    }
 }
