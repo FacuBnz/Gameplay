491c7748f56acbaad485219181198090e12c592f
FacuBnz <facu.j.benitez@gmail.com>
2021-06-13 21:36:13 -0300

    Se agrego la funcionalidad por si no existe la publicacion

--- html/Publicacion.php
+++ html/Publicacion.php
@@ -2,18 +2,27 @@
 include_once(__DIR__."/layout/header.php");
 include_once(__DIR__."/layout/sidebar.php");
 
-$titulo = $this->publicacion['titulo'];
-$fecha = $this->publicacion['fecha'];
-$catego = $this->publicacion['nombre'];
-$desc = $this->publicacion['descripcion'];
+if($this->publicacion){
+    $titulo = $this->publicacion['titulo'];
+    $fecha = $this->publicacion['fecha'];
+    $catego = $this->publicacion['nombre'];
+    $desc = $this->publicacion['descripcion'];
+}
+
+
 ?>
     <div id="principal">
-        <h1><?=$titulo?></h1>
+        <?php if($this->publicacion) : ?>
+            <h1><?=$titulo?></h1>
 
-        <article>
-            <span class="fecha"><?=$fecha?> | <?=$catego?></span>
-            <p><?=$desc?></p>
-        </article>
+            <article>
+                <span class="fecha"><?=$fecha?> | <?=$catego?></span>
+                <p><?=$desc?></p>
+            </article>
+        <?php else :?>
+            <h1>No se ha encontrado la publicación</h1>
+        <?php endif;?>
+        
     </div>
 </main>
 
