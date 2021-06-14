9d1c41f80980266792b4c9c0ea75c7186f7f0fcf
FacuBnz <facu.j.benitez@gmail.com>
2021-06-13 21:11:08 -0300

    Se modifico la descripcion de cada publicacion para que sea mas corto

--- html/Categoria.php
+++ html/Categoria.php
@@ -7,11 +7,13 @@ include_once(__DIR__."/layout/sidebar.php");
 
         <?php foreach ($this->publicaciones as $p): ?>
             <?php if ($this->nombreCategoria == $p['nombre']) :?>
+                
+                <?php $desc = str_replace(PHP_EOL, '<p>', substr($p["descripcion"], 0,200)); ?>
                 <article>
                     <a href="publicacion.php?titulo=<?=$p['titulo']?>">
                         <h2><?=$p['titulo']?></h2>
                         <span class="fecha"><?=$p['fecha']?> | <?=$p['nombre']?></span>
-                        <p><?=$p['descripcion']?></p>
+                        <p><?=$desc?>...</p>
                     </a>
                 </article>
             <?php endif; ?>
--- html/Index.php
+++ html/Index.php
@@ -6,16 +6,16 @@ include_once(__DIR__."/layout/sidebar.php");
         <h1>Ultimas publicaciones</h1>
 
         <?php foreach ($this->publicaciones as $p): ?>
+            
+            <?php $desc = str_replace(PHP_EOL, '<p>', substr($p["descripcion"], 0,200)); ?>
             <article>
                 <a href="publicacion.php?titulo=<?=$p['titulo']?>">
                     <h2><?=$p['titulo']?></h2>
                     <span class="fecha"><?=$p['fecha']?> | <?=$p['nombre']?></span>
-                    <p><?=$p['descripcion']?></p>
+                    <p><?=$desc?>...</p>
                 </a>
             </article>
         <?php endforeach;?>
-
-
 
         <div id="ver-todas">
             <a href="#">Ver todas</a>
