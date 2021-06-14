8057d08dbec89a238bc1769efecbd5055694eea5
FacuBnz <facu.j.benitez@gmail.com>
2021-06-14 00:08:15 -0300

    Se agregó la vista ver-todos

---
+++ html/VerTodos.php
@@ -0,0 +1,23 @@
+<?php
+include_once(__DIR__."/layout/header.php");
+include_once(__DIR__."/layout/sidebar.php");
+?>
+    <div id="principal">
+        <h1>Todas las publicaciones</h1>
+
+        <?php foreach ($this->publicaciones as $p): ?>
+            
+            <?php $desc = str_replace(PHP_EOL, '<p>', substr($p["descripcion"], 0,200)); ?>
+            <article>
+                <a href="publicacion.php?titulo=<?=$p['titulo']?>">
+                    <h2><?=$p['titulo']?></h2>
+                    <span class="fecha"><?=$p['fecha']?> | <?=$p['nombre']?></span>
+                    <p><?=$desc?>...</p>
+                </a>
+            </article>
+        <?php endforeach;?>
+    </div>
+</main>
+
+<?php
+include_once(__DIR__."/layout/footer.php");
