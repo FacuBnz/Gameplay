536cec1db6b07c21165dacb9b4218d136a88b7fd
FacuBnz <facu.j.benitez@gmail.com>
2021-06-14 00:06:37 -0300

    Se agrego el controlador ver-todos

---
+++ controllers/ver-todos.php
@@ -0,0 +1,20 @@
+<?php
+//controllers/index.php
+
+require '../fw/fw.php';
+require '../models/Categorias.php';
+require '../models/Publicaciones.php';
+require '../views/VerTodos.php';
+
+$cate = new Categorias();
+$cateTodos = $cate->getTodos();
+
+$publi = new Publicaciones();
+$publiTodos = $publi->getPublicaciones();
+
+//var_dump($publiTodos);
+
+$v = new VerTodos();
+$v->categorias = $cateTodos;
+$v->publicaciones = $publiTodos;
+$v->render();
