d71c916e6fd8ee6914b82af4e10ad12e2ecd5eb3
FacuBnz <facu.j.benitez@gmail.com>
2021-06-21 01:28:47 -0300

    Se agregó la funcionalidad de registro e inicio de sesion de usuarios

--- controllers/index.php
+++ controllers/index.php
@@ -4,7 +4,40 @@
 require '../fw/fw.php';
 require '../models/Categorias.php';
 require '../models/Publicaciones.php';
+require '../models/Usuarios.php';
 require '../views/Index.php';
+
+
+if(isset($_POST['registro'])){
+
+    if(!isset($_POST['nombre'])) die("Error de validacion nombre"); 
+    if(!isset($_POST['apellido'])) die("Error de validacion apellido"); 
+    if(!isset($_POST['emailr'])) die("Error de validacion email"); 
+    if(!isset($_POST['passwordr'])) die("Error de validacion password");
+    
+    try{
+        $user = new Usuarios();
+        $user->create($_POST['nombre'], $_POST['apellido'], $_POST['emailr'], $_POST['passwordr']);
+        $_SESSION['completo'] = "El registro se ha completado con exito";
+
+    }catch(ValidationUser $e){
+        $_SESSION['errores'] = $e;
+    }
+}
+
+if(isset($_POST['entrar'])){
+    
+    if(!isset($_POST['emaili'])) die("Error de validacion email"); 
+    if(!isset($_POST['passwordi'])) die("Error de validacion password"); 
+    
+    try {
+        $user = new Usuarios();
+        $_SESSION['usuario'] = $user->getUser($_POST['emaili'], $_POST['passwordi']);
+    } catch (ValidationUser $e) {
+        $_SESSION['errores'] = $e;
+    }
+}
+
 
 $cate = new Categorias();
 $cateTodos = $cate->getTodos();
@@ -17,4 +50,10 @@ $publiTodos = $publi->getPublicaciones(4);
 $v = new Index();
 $v->categorias = $cateTodos;
 $v->publicaciones = $publiTodos;
+$v->errores = isset($_SESSION['errores']) ? $_SESSION['errores'] : null;
+$v->completo = isset($_SESSION['completo']) ? $_SESSION['completo'] : null;
+$v->user = isset($_SESSION['usuario']) ? $_SESSION['usuario'] : null;
 $v->render();
+
+unset($_SESSION['completo']);
+unset($_SESSION['errores']);
