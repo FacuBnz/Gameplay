a3ac895ccadfe0e3651d0fd37fc68b8edb3e8cbf
FacuBnz <facu.j.benitez@gmail.com>
2021-06-21 01:23:12 -0300

    Se setea el timezone y la variable de sesion

--- fw/fw.php
+++ fw/fw.php
@@ -5,3 +5,9 @@
 require '../fw/Database.php';
 require '../fw/Model.php';
 require '../fw/View.php';
+
+date_default_timezone_set('America/Argentina/Buenos_Aires');
+
+if(!isset($_SESSION)){
+    session_start();
+}
