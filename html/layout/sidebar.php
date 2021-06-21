e08f26ad8867acd2fee1a7998f29a05e1f35f097
FacuBnz <facu.j.benitez@gmail.com>
2021-06-21 01:35:48 -0300

    Se agregó la funcionalidad de mostrar el menu de logueado y mensajes de ingresos invalidos

--- html/layout/sidebar.php
+++ html/layout/sidebar.php
@@ -1,46 +1,68 @@
 <main id="contenedor">
     <!-- sidebar -->
     <aside id="sidebar">
-        <!-- Si el usuario está logueado -->
 
-        <!--
-        <div id="login" class="bloque">
-          <h3>Bienvenido facu</h3>
-          <a href="#" class="boton boton-verde">Crear publicación</a>
-          <a href="#" class="boton boton">Crear categoria</a>
-          <a href="#" class="boton boton-turquesa">Mis publicaciones</a>
-          <a href="#" class="boton boton-naranja">Modificar datos</a>
-          <a href="#" class="boton boton-rojo">Cerrar sesion</a>
-        </div>
-        -->
         <!-- Login -->
-        <div id="login" class="bloque">
-            <h3>Identificate</h3>
-            <form action="#" method="POST">
-                <label for="emaili">Email</label>
-                <input type="email" name="emaili" id="emaili">
 
-                <label for="passwordi">Contraseña</label>
-                <input type="password" name="passwordi" id="passwordi">
-                <input type="submit" name="entrar" value="Entrar">
-            </form>
+        <?php if(isset($this->user)) :?>
+            <div id="login" class="bloque">
+            <h3>Bienvenido/a <?=$this->user["nombre"]?></h3>
+            <a href="#" class="boton boton-verde">Crear publicación</a>
+            <a href="#" class="boton boton">Crear categoria</a>
+            <a href="#" class="boton boton-turquesa">Mis publicaciones</a>
+            <a href="#" class="boton boton-naranja">Modificar datos</a>
+            <a href="cerrar.php" class="boton boton-rojo">Cerrar sesion</a>
         </div>
+        <?php else :?>
+            <div id="login" class="bloque">
+                <h3>Identificate</h3>
+                <?php if(isset($this->errores) && $this->errores->getTrace()[0]['function'] == 'getUser') : ?>
+                            <div class="alerta alerta-error"><?=$this->errores->getMessage()?></div>
+                    <?php endif ;?>
+                <form action="index.php" method="POST">
+                    <label for="emaili">Email</label>
+                    <input type="email" name="emaili" id="emaili">
 
-        <!-- Registro -->
+                    <label for="passwordi">Contraseña</label>
+                    <input type="password" name="passwordi" id="passwordi">
+                    <input type="submit" name="entrar" value="Entrar">
+                </form>
+            </div>
 
-        <div id="login" class="bloque">
-            <h3>Registrate</h3>
-            <form action="#" method="POST">
-                <label for="nombre">Nombre</label>
-                <input type="text" name="nombre" id="nombre">
-                <label for="apellido">Apellido</label>
-                <input type="text" name="apellido" id="apellido">
-                <label for="emailr">Email</label>
-                <input type="email" name="emailr" id="emailr">
-                <label for="passwordr">Contraseña</label>
-                <input type="password" name="passwordr" id="passwordr">
-                <input type="submit" name="registro" value="Registrar">
-            </form>
-        </div>
+            <!-- Registro -->
+
+            <div id="login" class="bloque">
+                <h3>Registrate</h3>
+                <?php if(isset($this->completo)) :?>
+                    <div class="alerta alerta-exito">
+                        <?=$this->completo?>
+                    </div>
+                <?php endif ;?>
+
+                <form action="index.php" method="POST">
+                    <?php if(isset($this->errores) && $this->errores->getTrace()[0]['function'] == 'create') : ?>
+                            <div class="alerta alerta-error"><?=$this->errores->getMessage()?></div>
+                    <?php endif ;?>
+                    <label for="nombre">Nombre</label>
+                    <input type="text" name="nombre" id="nombre">
+                    
+
+                    <label for="apellido">Apellido</label>
+                    <input type="text" name="apellido" id="apellido">
+
+
+                    <label for="emailr">Email</label>
+                    <input type="email" name="emailr" id="emailr">
+
+
+                    <label for="passwordr">Contraseña</label>
+                    <input type="password" name="passwordr" id="passwordr">
+
+                    <input type="submit" name="registro" value="Registrar">
+                </form>
+            </div>
+        <?php endif ;?>
+        
+        
 
     </aside>
