c17f30250c4e4c5807d4d96b5099824b67bb40d7
FacuBnz <facu.j.benitez@gmail.com>
2021-06-22 02:15:29 -0300

    Se agregó la funcionalidad de ver los mensajes de error o completo

--- html/MisPublicaciones.php
+++ html/MisPublicaciones.php
@@ -5,6 +5,16 @@ include_once(__DIR__."/layout/sidebar.php");
     <div id="principal">
         <h1>Mis publicaciones</h1>
 
+        <?php if(isset($this->completo)) :?>
+            <div class="alerta alerta-exito">
+                <?=$this->completo?>
+            </div>
+        <?php elseif(isset($this->errores)) : ?>
+            <div class="alerta alerta-error">
+                <?=$this->errores->getMessage()?>
+            </div>
+        <?php endif;?>
+
         <?php foreach ($this->publicaciones as $p): ?>
             
             <?php $desc = str_replace(PHP_EOL, '<p>', substr($p["descripcion"], 0,200)); ?>
@@ -15,7 +25,7 @@ include_once(__DIR__."/layout/sidebar.php");
                     <p><?=$desc?>...</p>
                 </a>
             </article>
-            <a href="modificar-publicacion.php?titulo=<?=$p['titulo']?>&categoria_id=<?=$p['categoria_id']?>&descripcion=<?=$p['descripcion']?>" class="boton boton-naranja\">Editar</a>
+            <a href="modificar-publicacion.php?titulo=<?=$p['titulo']?>" class="boton boton-naranja\">Editar</a>
             <a href="borrar-publicacion.php?titulo=<?=$p['titulo']?>" class="boton boton-rojo">Eliminar</a>
         <?php endforeach;?>
     </div>
