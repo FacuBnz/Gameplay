d0acb2784845d31c9d972a800ace48165ed8881a
FacuBnz <facu.j.benitez@gmail.com>
2021-06-22 02:08:43 -0300

    Se creo el controlador modificar-publicacion

---
+++ controllers/modificar-publicacion.php
@@ -0,0 +1,50 @@
+<?php
+
+require '../fw/fw.php';
+require '../models/Categorias.php';
+require '../models/Publicaciones.php';
+require '../views/ModificarPublicacion.php';
+
+if(!isset($_SESSION['usuario'])){
+    header('Location: index.php');
+}
+
+$cate = new Categorias();
+$publicacion = new Publicaciones();
+$v = new ModificarPublicacion();
+
+if(isset($_POST['modificar'])){
+    if(!isset($_POST['titulo'])) die("Error de validacion titulo");
+    if(!isset($_POST['titulo_anti'])) die("Error de validacion titulo");
+    if(!isset($_POST['categoria'])) die("Error de validacion categoria");
+    if(!isset($_POST['descripcion'])) die("Error de validacion descripcion");
+
+    try {
+        $publicacion->update($_POST['titulo'], $_POST['titulo_anti'], $_POST['categoria'], $_POST['descripcion'], $_SESSION['usuario']);
+        $_SESSION['completo_modificacion_post'] = "La publicacion se a guardado con exito";
+        header('Location: mis-publicaciones.php');
+    } catch (ValidationPost $e) {
+        $_SESSION['errores_modificacion_post'] = $e;
+        $v->publicacion = $publicacion->getPublicacionForUser($_SESSION['usuario'], $_POST['titulo_anti']);
+    }
+}
+
+$cateTodos = $cate->getTodos();
+$v->categorias = $cateTodos;
+
+if(isset($_GET['titulo'])){
+    try {
+        $v->publicacion = $publicacion->getPublicacionForUser($_SESSION['usuario'], $_GET['titulo']);
+    } catch (ValidationPost $e) {
+        $_SESSION['errores_modificacion_post'] = $e;
+    }
+}
+
+$v->errores = isset($_SESSION['errores_modificacion_post']) ? $_SESSION['errores_modificacion_post'] : null;
+$v->user = isset($_SESSION['usuario']) ? $_SESSION['usuario'] : null;
+$v->render();
+
+unset($_SESSION['errores_modificacion_post']);
+
+
+
