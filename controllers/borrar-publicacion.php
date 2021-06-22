1cd0927fef9e32c4b014f9a723142a64860a03e2
FacuBnz <facu.j.benitez@gmail.com>
2021-06-22 02:19:28 -0300

    Se agrego el controlador para borrar una publicacion

---
+++ controllers/borrar-publicacion.php
@@ -0,0 +1,20 @@
+<?php
+
+require '../fw/fw.php';
+require '../models/Publicaciones.php';
+
+if(!isset($_SESSION['usuario'])){
+    header('Location: index.php');
+}
+
+if(count($_GET) > 0){
+    try {
+        $publi = new Publicaciones();
+        $publi->delete($_GET['titulo'], $_SESSION['usuario']);
+        $_SESSION['completo_modificacion_post'] = "Publicación eliminada correctamente";
+    } catch (ValidationPost $e) {
+        $_SESSION['errores_modificacion_post'] = $e;
+    }
+}
+
+header('Location: mis-publicaciones.php');
